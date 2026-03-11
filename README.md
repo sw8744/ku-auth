2db91d084c683c0323ba084d60123cd564757721
thislife_won <sw8744@naver.com>
2026-03-11 11:31:51 +0900

    Finish Making

---
+++ README.md
@@ -0,0 +1,67 @@
+# 건국대학교 재학생 인증 API ku-auth
+![](https://i.namu.wiki/i/eKnJUBhT_j5ToOnlFN6LyZnjXDH6ABd0CWbO84AJn8U2nw6O2mzx33U-jD-8H2CzeqeZk0BpR7l7breQjGQEKD9s8rXV4ctjf2CgvLoG-nl0iRZVeOJE58yNeR88Zv4abvWyNpwMknIXMR9JkMbYPA.webp)
+
+# 개요
+이 API는 [건국대학교 ecampus](https://ecampus.konkuk.ac.kr/)의 로그인 시스템을 활용하여 건국대학교 재학생들을 대상으로 하는 서비스 개발에 유용하게 사용될 수 있도록 개발하였습니다.
+
+# 사용 방법
+`/api/ku_login` 주소로 `POST` 요청을 보냅니다. 이때 QueryString에 `id`와 `password` 값을 같이 해서 보냅니다.
+
+만일 로그인이 성공하였다면, 다음과 같은 결과값을 반환하게 됩니다. 이때, 반환된 정보는 ecampus에 등록된 사용자 정보를 반환합니다.
+```json
+{
+  "status_code": 200,
+  "detail": {
+    "student_number": "202612345",
+    "name": "김건국",
+    "contact": "01012345678",
+    "email": "ku@konkuk.ac.kr"
+  },
+  "headers": null
+}
+```
+
+만일 로그인이 실패하였다면, 다음과 같은 결과값을 반환하게 됩니다.
+```json
+{
+  "status_code": 401,
+  "detail": "incorrect id or password",
+  "headers": null
+}
+```
+
+만일 로그인이 5회 이상 실패하면 ecampus에서 해당 아이디의 로그인을**5분 동안 정지**를 시킵니다. 이때는 다음과 같은 결과값을 반환하게 됩니다.
+```json
+{
+  "status_code": 403,
+  "detail": "ban for 5 minutes",
+  "headers": null
+}
+```
+
+만일 `id` 값이 비어있다면, 다음과 같은 결과값을 반환하게 됩니다. 
+```json
+{
+  "status_code": 400,
+  "detail": "id is null",
+  "headers": null
+}
+```
+
+만일 `password` 값이 비어있다면, 다음과 같은 결과값을 반환하게 됩니다.
+```json
+{
+  "status_code": 400,
+  "detail": "password is null",
+  "headers": null
+}
+```
+
+만일 `id`와 `password` 값이 동시에 비어있다면, 다음과 같은 결과값을 반환하게 됩니다.
+```json
+{
+  "status_code": 400,
+  "detail": "id and password is null",
+  "headers": null
+}
+```
---
+++ main.py
@@ -0,0 +1,95 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+import json
+from bs4 import BeautifulSoup
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], allow_credentials=True,
+    allow_methods=["*"], allow_headers=["*"],
+)
+
+@app.get("/api/ku_login")
+def ku_login(id: str, password: str):
+    if id == "" and password == "":
+        return HTTPException(
+            status_code=400,
+            detail="id and password is null"
+        )
+
+    elif id == "":
+        return HTTPException(
+            status_code=400,
+            detail="id is null"
+        )
+
+    elif password == "":
+        return HTTPException(
+            status_code=400,
+            detail="password is null"
+        )
+
+    session = requests.Session()
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
+    }
+
+    session.post("https://ecampus.konkuk.ac.kr/ilos/lo/logout.acl", timeout=10)
+
+    body = {
+        "usr_id": id,
+        "usr_pwd": password,
+        "campus_div": "1",
+        "encoding": "utf-8"
+    }
+
+    login_response = session.post(url="https://ecampus.konkuk.ac.kr/ilos/lo/login.acl?data=jsonLogin",
+               headers=headers,
+               data=body
+               )
+
+    login_response_json = json.loads(login_response.text.strip()[11:-2])
+
+    if login_response_json["isError"]:
+        if login_response_json["message"] == "ID 또는 비밀번호를 5회이상 잘못 입력하셨습니다. 5분 후에 다시 접속하시기 바랍니다.":
+            return HTTPException(
+                status_code=403,
+                detail="ban for 5 minutes"
+            )
+
+        return HTTPException(
+            status_code=401,
+            detail="incorrect id or password"
+        )
+
+    info_response = session.get(
+        url="https://ecampus.konkuk.ac.kr/ilos/mp/myinfo_form.acl",
+        headers=headers
+    )
+
+    info_html = BeautifulSoup(info_response.text, "html.parser")
+
+    tds = []
+
+    for td in info_html.select("td"):
+        tds.append(td.text.strip())
+
+    info = {
+        "student_number": tds[1][-10:-1],
+        "name": tds[1][:-11],
+        "contact": tds[4],
+        "email": tds[6]
+    }
+
+    return HTTPException(
+        status_code=200,
+        detail=info
+    )
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5001)
